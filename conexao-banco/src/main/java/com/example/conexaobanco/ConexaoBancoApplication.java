a29d05d7f4f570c5b986aec5d5d3f896a724fba3
Matheus Marciano <marciannoooo0@gmail.com>
2023-02-06 13:59:24 -0300

    new project

---
+++ conexao-banco/src/main/java/com/example/conexaobanco/ConexaoBancoApplication.java
@@ -0,0 +1,13 @@
+package com.example.conexaobanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConexaoBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConexaoBancoApplication.class, args);
+	}
+
+}
---
+++ conexao-banco/src/test/java/com/example/conexaobanco/ConexaoBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conexaobanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConexaoBancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
