2afb4b9ca0b6c089faba839088badb10c49c9d8b
Matheus Marciano <marciannoooo0@gmail.com>
2023-02-08 16:03:57 -0300

    adding new project to repository

---
+++ projeto-spring-produtos/src/main/java/com/example/projetospringprodutos/ProjetoSpringProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetospringprodutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringProdutosApplication.class, args);
+	}
+
+}
---
+++ projeto-spring-produtos/src/test/java/com/example/projetospringprodutos/ProjetoSpringProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetospringprodutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
