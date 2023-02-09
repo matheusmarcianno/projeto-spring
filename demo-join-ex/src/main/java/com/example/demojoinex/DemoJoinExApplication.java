55681a851906518a3b372cf2fdbf4b83d61d5a35
Matheus Marciano <marciannoooo0@gmail.com>
2023-02-09 15:47:34 -0300

    add new project

---
+++ demo-join-ex/src/main/java/com/example/demojoinex/DemoJoinExApplication.java
@@ -0,0 +1,13 @@
+package com.example.demojoinex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJoinExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJoinExApplication.class, args);
+	}
+
+}
---
+++ demo-join-ex/src/test/java/com/example/demojoinex/DemoJoinExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demojoinex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJoinExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
