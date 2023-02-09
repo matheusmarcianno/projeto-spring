0fe448552880043a7711e9eccdec08ff1ea550e2
Matheus Marciano <marciannoooo0@gmail.com>
2023-02-09 14:18:34 -0300

    adding new project

---
+++ Join-Exc-API/src/test/java/com/example/JoinExcAPI/JoinExcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JoinExcAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinExcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
